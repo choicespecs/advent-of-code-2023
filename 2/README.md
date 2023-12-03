f4d50316350794da351655c1932e8175e816b770
christophermlee2 <christophermlee2@gmail.com>
2023-12-03 15:38:30 -0500

    Day 2: Part 2 Complete

--- 2/README.md
+++ 2/README.md
@@ -7,3 +7,9 @@ Ids are lines within a file given 3 sets of cubes which are separated via semico
 - Hashmap count each color to number within a set
 - Once this has happened just compare between the given color,number values to determine if this is possible or not
 - Impossible sets means entire game is not valid so can continue to next line / game
+
+Finding the minimum number of color necessary between sets means finding the max possible for a specific color within a set.
+- Perform the same split between game sets as in part 1
+- Check for the max needed for sets
+- Calculate the power and place into an array
+- Sum the array at the end
--- 2/ans.py
+++ 2/ans.py
@@ -31,3 +31,27 @@ def part1():
                 ids.append(id)
             id += 1
     return sum(ids)
+
+def part2():
+    power = []
+    with open(INPUT_FILE, 'r') as file:
+        for line in file:
+            line = line.split(':')[1]
+            sets = line.split(';')
+            hm = {"red": 0, "green": 0, "blue": 0}
+            for set in sets:
+                set_cols = set.split(',')
+                compare = {"red": 0, "green": 0, "blue": 0}
+                for col in set_cols:
+                    single_col = col.split()
+                    compare[single_col[1]] += int(single_col[0])
+                for k, v in compare.items():
+                    if compare[k] > hm[k]:
+                        hm[k] = v
+            pwr = 1
+            for k,v in hm.items():
+                pwr *= v
+            power.append(pwr)
+    return sum(power)
+
+
